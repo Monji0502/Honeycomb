f21966309ff14dc7ab21ddf088418a250ae08426
Monji0502 <shizuto1031@gmail.com>
2020-01-07 20:05:47 +0900

    first commit

---
+++ Honeycomb/Honeycomb/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  Honeycomb
+//
+//  Created by 中谷静斗 on 2019/12/24.
+//  Copyright © 2019 中谷静斗. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func sceneDidLoad() {
+
+        
+        }
+        
+    
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+
+}
